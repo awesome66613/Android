0d112b96af6857fc18ce7d01a36c7ba7d531dfc1
awesome66613 <nechepurnov.m@gmail.com>
2021-02-18 13:51:28 +0300

    HomeWork 3.2-3

--- HomeWork3.1/src/Apple.java
+++
@@ -1,6 +0,0 @@
-public class Apple extends Fruit {
-    @Override
-    float getWeight() {
-        return 1.0f;
-    }
-}
--- HomeWork3.1/src/Orange.java
+++
@@ -1,6 +0,0 @@
-public class Orange extends Fruit {
-    @Override
-    float getWeight() {
-        return 1.5f;
-    }
-}
